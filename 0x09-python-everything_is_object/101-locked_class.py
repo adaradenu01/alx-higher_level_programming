9cad476224998e3b7245898b90b4b34af9fe609a
Ogungbemi Gbenga <adaradenu01@gmail.com>
2023-04-07 11:46:42 -0700

    python3

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,6 +3,8 @@
 
 
 class LockedClass:
-    """Prevent the user from creating new attributes in the LockedClass except for the declared attributes called 'first_name'"""
+    """
+    Prevent the user from creating new attributes in the Locked      Class except for the declared attributes called 'first_name
+    '"""
 
     __slots__ = ["first_name"]
