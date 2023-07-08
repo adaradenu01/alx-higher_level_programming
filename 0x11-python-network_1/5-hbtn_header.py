538ca5196fa1a71440db43713e1b6bb4eaec8431
Ogungbemi Gbenga <adaradenu01@gmail.com>
2023-07-08 03:09:21 -0700

    retrieving HTTP requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and
-displays the value of the variable X-Request-Id in the response header"""
+"""Get value of `X-Request-Id` header from requested URL"""
 
 if __name__ == "__main__":
-    import requests
-    import sys
+    from requests import get
+    from sys import argv
 
-url = sys.argv[1]
-try:
-    response = requests.get(url)
-    print(response.headers.get('X-Request-Id'))
-
-except Exception as e:
-    print ('e')
+    try:
+        r = get(argv[1])
+        print(r.headers.get('X-Request-Id'))
+    except Exception as e:
+        print(e)
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter,
-and finally displays the body of the response"""
+"""Post an email address to the requested URL"""
 
 if __name__ == "__main__":
-    import requests
-    import sys
-    
-url, email = sys.argv[1], sys.argv[2]
-try:
-    response = requests.post(url, data={'email': email})
-    print(response.text)
-except Exception as e:
-    print(e)
+    from requests import post
+    from sys import argv
+
+    try:
+        r = post(argv[1], data={'email': argv[2]})
+        print(r.text)
+    except Exception as e:
+        print(e)
