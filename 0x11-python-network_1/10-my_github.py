8784156812239eeda150e506aa19bb4681732c1e
Ogungbemi Gbenga <adaradenu01@gmail.com>
2023-07-08 01:16:29 -0700

    still fixing the errors

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,6 +11,6 @@ url = "https://api.github.com/user"
 try:
     response = requests.get(url, auth=(username, password))
     response.raise_for_status()
-    print(f"Your user ID is: {response.json()['id']}")
-except requests.exceptions.RequestException as e:
+    print(f"{response.json()['id']}")
+except Exception as e:
     print(f"Error: {e}")
