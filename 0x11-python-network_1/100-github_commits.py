7329753ee44aad6e4c5e9ad59df2eacec0ce4ee9
Ogungbemi Gbenga <adaradenu01@gmail.com>
2023-07-08 01:10:02 -0700

    still fixing the errors

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,17 +6,18 @@ format: `<sha>: <author name>`
 """
 
 if __name__ == "__main__":
-    import requests
-    import sys
+    from requests import get
+    from sys import argv
 
-repo_name, owner_name = sys.argv[1], sys.argv[2]
-url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-params = {"author": owner_name, "per_page": 10}
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
 
-try:
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    for commit in response.json():
-        print(f"{commit['sha']}: {commit['commit']['author']['name']}")
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+    try:
+        r = get(url)
+        j = r.json()
+        for commit in j[:10]:
+            print('{}: {}'.format(commit.get('sha'),
+                                  commit.get('commit')
+                                  .get('author')
+                                  .get('name')))
+    except IndexError as e:
+        print(e)
